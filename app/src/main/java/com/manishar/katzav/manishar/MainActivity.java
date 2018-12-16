13303921ea4472ac0315bc35db9b1b99660ac8a2
Gabi Kazav <gabik@google.com>
2018-12-16 19:31:00 +0200

    Fix Sheets API access

--- app/src/main/java/com/manishar/katzav/manishar/MainActivity.java
+++ app/src/main/java/com/manishar/katzav/manishar/MainActivity.java
@@ -1,27 +1,52 @@
 package com.manishar.katzav.manishar;
 
+import android.Manifest;
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.IOException;
+import java.util.Arrays;
+
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
 
     private Sheets service;
     Spinner spinner_category;
+    GoogleAccountCredential mCredential;
     private Button btnSubmit;
     private EditText editText_amount, editText_comments;
     private String spreadsheetId = "1DKNC8Rsd7Wqav59wblqbCK6WU9IDdnHQm7qOjhgJxy4";
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
 
 
     @Override
@@ -35,17 +60,136 @@ public class MainActivity extends AppCompatActivity {
         addListenerOnButton();
         addListenerOnSpinnerItemSelection();
 
-        String range = "B45:B45";
-        ValueRange result;
-        try {
-            result = service.spreadsheets().values().get(spreadsheetId, range).execute();
-            int numRows = result.getValues() != null ? result.getValues().size() : 0;
-            System.out.printf("%d rows retrieved.", numRows);
-        } catch (IOException e) {
-            System.out.printf("Error reading cell");
-            e.printStackTrace();
-        }
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SheetsScopes.SPREADSHEETS))
+                .setBackOff(new ExponentialBackOff());
 
+        getAccount();
+
+    }
+
+    private class runOnSheets extends Thread {
+        public void run() {
+            String range = "B45:B45";
+            ValueRange result;
+            try {
+                service = getSheetsService();
+                result = service.spreadsheets().values().get(spreadsheetId, range).execute();
+                int numRows = result.getValues() != null ? result.getValues().size() : 0;
+                Log.i("SHEETS", Integer.toString(numRows) + " rows retrieved.");
+            } catch (UserRecoverableAuthIOException e) {
+                startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
+            } catch (IOException e) {
+                Log.e("SHEETS", "Error reading cell");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void chooseAccount() {
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.GET_ACCOUNTS)) {
+            String accountName = getPreferences(Context.MODE_PRIVATE)
+                    .getString(PREF_ACCOUNT_NAME, null);
+            if (accountName != null) {
+                mCredential.setSelectedAccountName(accountName);
+                getAccount();
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(
+                        mCredential.newChooseAccountIntent(),
+                        REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs to access your Google account.",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    Manifest.permission.GET_ACCOUNTS);
+        }
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(this);
+        return connectionStatusCode == ConnectionResult.SUCCESS;
+    }
+
+    private void acquireGooglePlayServices() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(this);
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        Dialog dialog = apiAvailability.getErrorDialog(
+                MainActivity.this,
+                connectionStatusCode,
+                REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+
+    private void getAccount() {
+        if (! isGooglePlayServicesAvailable()) {
+            acquireGooglePlayServices();
+        } else if (mCredential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else {
+            runOnSheets sheets_thread = new runOnSheets();
+            //sheets_thread.run();
+            sheets_thread.start();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    Toast.makeText(this,"This app requires Google Play Services. Please install Google Play Services on your device and relaunch this app.", Toast.LENGTH_LONG);
+                } else {
+                    getAccount();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        SharedPreferences settings =
+                                getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                        mCredential.setSelectedAccountName(accountName);
+                        getAccount();
+                    }
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == RESULT_OK) {
+                    getAccount();
+                }
+                break;
+
+        }
+    }
+
+
+    private Sheets getSheetsService() {
+        HttpTransport transport = AndroidHttp.newCompatibleTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        return new Sheets.Builder(transport, jsonFactory, mCredential)
+                .setApplicationName("MaNishar")
+                .build();
     }
 
     // add items into spinner dynamically
@@ -88,17 +232,5 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-    }
-
-
-
-    public ValueRange getValues(String spreadsheetId, String range) throws IOException {
-        Sheets service = this.service;
-        // [START sheets_get_values]
-        ValueRange result = service.spreadsheets().values().get(spreadsheetId, range).execute();
-        int numRows = result.getValues() != null ? result.getValues().size() : 0;
-        System.out.printf("%d rows retrieved.", numRows);
-        // [END sheets_get_values]
-        return result;
     }
 }
